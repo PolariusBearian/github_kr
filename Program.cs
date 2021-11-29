60628995051de83ba43ea4647cc1c4fc6ec412b9
PeNgu1n4ik <mishkanednishka2@gmail.com>
2021-11-29 07:31:24 +0300

    "Была добавлена задача с умножением"

--- Program.cs
+++ Program.cs
@@ -7,7 +7,15 @@ namespace Git_kr
         public static void Main(string[] args)
         {
             DateTime today = DateTime.Today;
+            int answer;
             Console.WriteLine("Добрый день," + Environment.UserName + "сегодня " + today.ToString("D"));
+            Console.WriteLine("Сколько будет 2 умножить на 18");
+            answer = Convert.ToInt32(Console.ReadLine());
+            if (answer != 36) Console.WriteLine("Вы не правы!");
+            else
+            {
+                Console.WriteLine("Вы молодчина!");
+            }
         }
     }
 }
